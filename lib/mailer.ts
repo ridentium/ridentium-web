bfa30d1687e3ec0af3d42525f43e6305c776c96f
ridentium <ridentium@gmail.com>
2026-04-16 22:22:34 +0200

    fix: usa named import createTransport (compatibilità TypeScript)

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -1,6 +1,6 @@
-import nodemailer from 'nodemailer'
+import { createTransport } from 'nodemailer'
 
-const transporter = nodemailer.createTransport({
+const transporter = createTransport({
   service: 'gmail',
   auth: {
     user: process.env.GMAIL_USER,
